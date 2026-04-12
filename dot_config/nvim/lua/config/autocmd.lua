23d5d2ceb430cc0e41d1e90b4473e0dc64c467c3
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2026-04-13 01:23:18 +0300

    wip

--- dot_config/nvim/lua/config/autocmd.lua
+++ dot_config/nvim/lua/config/autocmd.lua
@@ -63,7 +63,7 @@ vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave', 'BufEnter
 
 -- no auto continue comments on new line
 vim.api.nvim_create_autocmd('FileType', {
-  group = vim.api.nvim_create_augroup('no_auto_comment', {}),
+  group = vim.api.nvim_create_augroup('no_auto_comment', { clear = true }),
   callback = function()
     vim.opt_local.formatoptions:remove { 'c', 'r', 'o' }
   end,
