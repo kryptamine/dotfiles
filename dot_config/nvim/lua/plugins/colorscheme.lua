c94e481e8fa48e0aa7f4a8f1f79c437c5b7bb7f6
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2026-08-08 15:26:30 +0300

    wip

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,36 +1,49 @@
 return {
+	-- {
+	-- 	"https://github.com/qwlp/gruber-darker.nvim",
+	-- 	name = "gruber-darker",
+	-- 	config = function()
+	-- 		require("gruber-darker").setup({
+	-- 			bold = false,
+	-- 			undercurl = true,
+	-- 			underline = true,
+	-- 			invert = {
+	-- 				signs = false,
+	-- 				tabline = false,
+	-- 				visual = false,
+	-- 			},
+	-- 			italic = {
+	-- 				strings = false,
+	-- 				comments = false,
+	-- 				operators = false,
+	-- 				folds = true,
+	-- 			},
+	-- 		})
+	-- 		vim.api.nvim_set_hl(0, "@punctuation.delimiter.jsdoc", { fg = "#73D936" })
+	-- 		vim.cmd.colorscheme("gruber-darker")
+	--
+	-- 		local bgColor = "#000000"
+	-- 		local labelBg = "#FFDD33"
+	--
+	-- 		vim.api.nvim_set_hl(0, "SnacksPickerTitle", { fg = bgColor, bg = labelBg })
+	-- 		vim.api.nvim_set_hl(0, "SnacksPickerInputTitle", { fg = bgColor, bg = labelBg })
+	-- 		vim.api.nvim_set_hl(0, "SnacksPickerPreviewTitle", { fg = bgColor, bg = labelBg })
+	-- 		vim.api.nvim_set_hl(0, "SnacksPickerListTitle", { fg = bgColor, bg = labelBg })
+	-- 		vim.api.nvim_set_hl(0, "SnacksPickerBorder", { bg = bgColor, fg = "#323232" })
+	-- 		vim.api.nvim_set_hl(0, "SnacksPicker", { bg = bgColor })
+	-- 	end,
+	-- },
 	{
-		"https://github.com/qwlp/gruber-darker.nvim",
-		name = "gruber-darker",
+		"webhooked/kanso.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
 		config = function()
-			require("gruber-darker").setup({
-				bold = false,
-				undercurl = true,
-				underline = true,
-				invert = {
-					signs = false,
-					tabline = false,
-					visual = false,
-				},
-				italic = {
-					strings = false,
-					comments = false,
-					operators = false,
-					folds = true,
-				},
+			require("kanso").setup({
+				minimal = true,
 			})
-			vim.api.nvim_set_hl(0, "@punctuation.delimiter.jsdoc", { fg = "#73D936" })
-			vim.cmd.colorscheme("gruber-darker")
 
-			local bgColor = "#000000"
-			local labelBg = "#FFDD33"
-
-			vim.api.nvim_set_hl(0, "SnacksPickerTitle", { fg = bgColor, bg = labelBg })
-			vim.api.nvim_set_hl(0, "SnacksPickerInputTitle", { fg = bgColor, bg = labelBg })
-			vim.api.nvim_set_hl(0, "SnacksPickerPreviewTitle", { fg = bgColor, bg = labelBg })
-			vim.api.nvim_set_hl(0, "SnacksPickerListTitle", { fg = bgColor, bg = labelBg })
-			vim.api.nvim_set_hl(0, "SnacksPickerBorder", { bg = bgColor, fg = "#323232" })
-			vim.api.nvim_set_hl(0, "SnacksPicker", { bg = bgColor })
+			vim.cmd.colorscheme("kanso-zen")
 		end,
 	},
 }
